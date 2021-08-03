e45f275a0cb3eece3cbc2363f8943a97549aef36
Calum Mackervoy <c.mackervoy@gmail.com>
2021-08-03 17:58:08 +0200

    feat: loginForm component

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,1 @@
-export { addition } from "./util/addition";
-export { subtraction } from "./util/subtraction";
-
-export { HelloWorld } from "./components/hello-world/HelloWorld";
+export { LoginForm } from './components/loginForm/LoginForm'
--- src/util/addition.spec.ts
+++
@@ -1,7 +0,0 @@
-import { addition } from "./addition";
-
-it("adds two numbers together", () => {
-  const result = addition(5, 3);
-
-  expect(result).toBe(8);
-});
