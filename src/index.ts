d85c80600c388cb6028d9341e5448b1b3bf98a22
Calum Mackervoy <c.mackervoy@gmail.com>
2021-08-07 19:40:48 +0200

    feat: added MUD lit to utils

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { LoginForm } from "./components/loginForm/LoginForm";
 export * from "./components/thingList/ThingList";
+export * from "./util/lit";
 export { ThingDepiction } from "./components/thingDepiction/ThingDepiction";
