e4ff13bdab7b591695e6edec2873b80ccc3a4cda
Calum Mackervoy <c.mackervoy@gmail.com>
2021-08-07 19:46:42 +0200

    feat: added RDF utils

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { LoginForm } from "./components/loginForm/LoginForm";
 export * from "./components/thingList/ThingList";
 export * from "./util/lit";
+export * from "./util/rdf";
 export { ThingDepiction } from "./components/thingDepiction/ThingDepiction";
