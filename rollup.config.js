8cfaa3652d24a4c9d514274b4a351ef487e2c719
Calum Mackervoy <c.mackervoy@gmail.com>
2021-08-08 19:49:15 +0200

    build: typescript plugin for rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
+import typescript from "@rollup/plugin-typescript";
 import sucrase from "@rollup/plugin-sucrase";
 
 export default {
   input: "src/index.ts",
   plugins: [
+    typescript({ tsconfig: "./tsconfig.json" }),
     sucrase({
       exclude: ["node_modules/*"],
       transforms: ["jsx", "typescript"],
