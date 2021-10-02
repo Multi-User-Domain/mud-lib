ad5914211fde7e7f572c1c21668fe31e96a36a63
Calum Mackervoy <c.mackervoy@gmail.com>
2021-10-02 12:46:24 +0200

    build(rollup): replace sucrase with babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,37 @@
 import typescript from "@rollup/plugin-typescript";
-import sucrase from "@rollup/plugin-sucrase";
+import commonjs from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
+import replace from "@rollup/plugin-replace";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import json from "@rollup/plugin-json";
 
 export default {
   input: "src/index.ts",
   plugins: [
     typescript({ tsconfig: "./tsconfig.json" }),
-    sucrase({
-      exclude: ["node_modules/*"],
-      transforms: ["jsx", "typescript"],
+    nodeResolve({
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+      module: true,
+      browser: true,
     }),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("development"),
+    }),
+    babel({
+      presets: [
+        ["@babel/preset-env", { targets: { node: "current" } }],
+        //["@babel/preset-react", {"runtime": "automatic"}],
+        "@babel/preset-react",
+        "@babel/preset-typescript",
+      ],
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+      exclude: /node_modules/,
+    }),
+    commonjs({
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+      include: /node_modules/,
+    }),
+    json(),
   ],
   external: ["react"],
   output: [
--- src/context/mudAccountContext/index.tsx
+++ src/context/mudAccountContext/index.tsx
@@ -1,4 +1,4 @@
-import {
+import React, {
   ReactElement,
   createContext,
   useState,
