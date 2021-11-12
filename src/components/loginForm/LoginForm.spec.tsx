37dda043c3fe0dfeb465ecc34be821e839339d34
Matt Tennison <matt@matttennison.com>
2021-11-12 12:32:47 +0000

    chore(core): correct file names for main & module in package.json

--- src/components/loginForm/LoginForm.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-
-import { LoginForm } from "./LoginForm";
-
-it("renders", () => {
-  const { asFragment } = render(<LoginForm />);
-  expect(asFragment()).toMatchSnapshot();
-});
